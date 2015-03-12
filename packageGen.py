688f8c498d4b960c180926f0302ba20fca89622d
mbrookes <brookesy+git@gmail.com>
2015-03-12 09:38:31 +0000

    Reformatted some loops and added more comments.

--- packageGen.py
+++ packageGen.py
@@ -5,51 +5,55 @@ from collections import defaultdict
 import click
 
 
-#Boring ole object to hold some details
+# Boring ole object to hold some details
 class MetadataType(object):
     filename = ""
     xmlname = ""
 
+
 #Create something way over complicated to get all the files and their xmlname mappings
 def create_filelist(path):
-
+    #Scan the main dir to find the folders in it
     directorymapping = mappingHelper.directorymapping()
     directories = []
     for entry in scandir.scandir(path):
         if not entry.name.startswith('.') and entry.is_dir() and entry.name in mappingHelper.directorynames():
             directories.append(entry)
 
+    #Scan each of the dirs found from the main dir
     directorydict = {}
     for directory in directories:
         directorydict[directory.name] = scandir.scandir(directory.path)
 
+    #Create a dict and fill it with directories and metadata objects
     filesdict = defaultdict(list)
     for directory, directorydetails in directorydict.items():
         for x in directorydetails:
             try:
-                if x.name.split('.')[1] in mappingHelper.suffixnames():
-                    filemeta = MetadataType()
-                    filemeta.filename = x.name.split('.')[0]
-                    filemeta.xmlname = directorymapping.get(directory)
-                    filesdict[directorymapping.get(directory)].append(filemeta)
+                suffixname = x.name.split('.')[1]
             except:
-                print("File error, more than likely something folder based like reports")
+                suffixname = None
+                print("Suffix not found or no suffix for {}, more than likely something folder based like reports".format(x.name))
+            if suffixname in mappingHelper.suffixnames():
+                filemeta = MetadataType()
+                filemeta.filename = x.name.split('.')[0]
+                filemeta.xmlname = directorymapping.get(directory)
+                filesdict[directorymapping.get(directory)].append(filemeta)
 
     return filesdict
 
+
 #Create the package file.
 def create_package(files):
-
     #create package.xml
     packageNS = "http://soap.sforce.com/2006/04/metadata"
-    packageNsmap = { None : packageNS}
+    packageNsmap = {None: packageNS}
     package = etree.Element("Package", nsmap=packageNsmap)
 
     for k, v in files.items():
         types = etree.SubElement(package, "types")
 
         for row in v:
-
             members = etree.SubElement(types, "members")
             members.text = row.filename
 
@@ -63,11 +67,13 @@ def create_package(files):
     #print(etree.tostring(packageXML, encoding='utf8', method='xml'))
     packageXML.write("package.xml", pretty_print=True, xml_declaration=True)
 
+
 @click.command()
 @click.option('--path', default='.', help='Path of the directory to scan')
 def run(path):
     files = create_filelist(path)
     create_package(files)
+
 
 if __name__ == '__main__':
     run()
